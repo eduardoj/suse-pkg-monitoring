08e0ba6e07b4252c2ed09be85dea2a79cce82230
Eduardo J <eduardoj@gmail.com>
2016-01-07 20:19:11 +0000

    Change identation from 2 to 3 spaces

--- lib/PkgMonitoring.pm
+++ lib/PkgMonitoring.pm
@@ -5,18 +5,18 @@ use Mojolicious::Plugin::Config;
 
 
 sub startup {
-  my $self = shift;
+   my $self = shift;
 
-  $self->plugin('Config');
+   $self->plugin('Config');
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+   # Documentation browser under "/perldoc"
+   $self->plugin('PODRenderer');
 
-  # Router
-  my $r = $self->routes;
+   # Router
+   my $r = $self->routes;
 
-  # Normal route to controller
   $r->get('/')->to('example#welcome');
+   # Normal route to controller
 }
 
 1;
